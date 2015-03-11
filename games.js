09d065b6c9bb5674ebe8481c58d67672936df09e
William Waites <wwaites@tardis.ed.ac.uk>
2015-03-11 22:34:14 +0000

    mostly cosmetic webby stuff

--- games.js
+++ games.js
@@ -275,7 +275,6 @@ function die(d) {
 }
 
 function fitness(d) {
-//    j = Object.keys(d.food).length / production_window;
     var n = 0;
     var j = 0;
     for(var i in d.food) {
@@ -335,6 +334,53 @@ function mean(arr) {
     return m;
 }
 
+function setup_payoff_plot() {
+    var m = [0, 10, 25, 10];
+    var w = 250 - m[1] - m[3];
+    var h = 150 - m[0] - m[2];
+    var xscale = d3.scale.linear().range([0, w]);
+    var yscale = d3.scale.linear().range([h, 0]);
+
+    var line = d3.svg.line()
+	.x(function (d) { return xscale(d.x); })
+	.y(function (d) { return yscale(d.y); })
+	.interpolate("basis");
+
+    var plot = d3.select("#payoff")
+	.append("svg")
+	.attr("width", w + m[1] + m[3])
+	.attr("height", h + m[0] + m[2])
+	.append("svg:g");
+
+    plot.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+    var xaxis = d3.svg.axis().scale(xscale).ticks(4);
+    plot.append("svg:g")
+	.style("stroke", "#000")
+	.style("fill", "none")
+	.attr("transform", "translate(0," + h + ")")
+	.call(xaxis);
+
+    var payoff = plot.append("path")
+	.style("stroke", "blue")
+	.style("stroke-width", 1)
+	.style("fill", "none");
+
+    var update = function (n, x_n) {
+	if (n == 0) n = 1;
+	var samples = d3.range(0, x_n).map(function (x) {
+	    return {
+		x: x/x_n,
+		y: 1/(1 + Math.exp(-30 * (x/n - 1/2)))
+	    }
+	});
+	payoff.attr("d", line(samples));
+    }
+
+    return update;
+}
+var update_payoff_plot = setup_payoff_plot();
+
 function update_counts() {
     d3.select("#cells").text(circle.size());
     d3.select("#cheaters").text(
@@ -346,6 +392,7 @@ function update_counts() {
     var coopfit = [];
     var cheatfit = [];
     var fits = [];
+    var food = [];
     circle.each(function (d) {
 	var fit = fitness(d);
 	if (d.kind == "coop")
@@ -354,14 +401,25 @@ function update_counts() {
 	    cheatfit.push(fit);
 	fits.push(fit);
 	nsize.push(Object.keys(d.food).length);
+	j = 0;
+	for(var i in d.food) {
+	    j = j + d.food[i].amount;
+	}
+	food.push(j);
     });
-    d3.select("#nsize").text(
+    d3.select("#neighbourhood").text(
 	sprintf("%d/%.02f/%d", d3.min(nsize), mean(nsize), d3.max(nsize))
     );
     d3.select("#fitness").text(
 	sprintf("%.02f/%.02f/%.02f", mean(coopfit), mean(cheatfit), mean(fits))
     );
-    d3.timer(update_counts, 1);
+    d3.select("#food").text(
+	sprintf("%.02f/%.02f/%.02f", d3.min(food), mean(food), d3.max(food))
+    );
+    d3.timer(update_counts, 1000);
+
+    update_payoff_plot(mean(nsize), d3.max(food));
+
     return true;
 }
-d3.timer(update_counts, 1);
+d3.timer(update_counts, 1000);
